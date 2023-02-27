a87ff593cdeb4177c67c9dd90e0746a4d5d031b8
weily2023 <126380220+weily2023@users.noreply.github.com>
2023-02-27 17:50:29 +0800

    Update consts.js

--- src/consts.js
+++ src/consts.js
@@ -1,5 +1,5 @@
-export const SITE_TITLE = `Austin's Blog`;
+export const SITE_TITLE = `郸城医梦's Blog`;
 export const SITE_DESCRIPTION = 'Austin Site Description';
-export const SITE_EMAIL = 'no.sql@qq.com'
-export const SITE_NAME = 'astro-blog.qum.cc';
-export const SITE_URL = "https://astro-blog.qum.cc";
+export const SITE_EMAIL = '328744221@qq.com'
+export const SITE_NAME = 'dcym365.cn';
+export const SITE_URL = "https://dcym365.cn";
